2e8e490a8ad183bf50e9d6e4d8d609fdaecaca2c
Nathan Preuss <nathan.b.preuss-1@ou.edu>
2020-10-11 13:03:43 -0500

    up to lab 8, most of project2

---
+++ R/Fire.r
@@ -0,0 +1,11 @@
+#' Fire damage
+#'
+#' A data set collected concerning the cst of home damage from fire
+#'
+#' @format A data frame with 15 rows and 2 variables:
+#' \describe{
+#' \item{DISTANCE}{Distance of the house from the fire station in miles}
+#' \item{DAMAGE}{Damage in thousands of dollars}
+#' }
+#' @source \url{https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sincich/p/book/9781498728850}
+"fire"
