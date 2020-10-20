1a469b53e52c7e242904a8c44379c5679e74b264
Nathan Preuss <nathan.b.preuss-1@ou.edu>
2020-10-20 11:43:49 -0500

    lab9

---
+++ R/nathanboot2.R
@@ -0,0 +1,44 @@
+#' Create a Bootstrap Confidence Interval for a given sample
+#'
+#' More detailed description
+#'
+#' @param iter the number of iteration to sample from
+#' @param x the sample from which data will be sampled
+#' @param fun the name of the function to be applied to the data, for example, mean, sd, median, etc...
+#' @param alpha the alpha value used to construct confidence intervals of size (1-alpha)*100
+#' @param cx numerical character expansion factor
+#' @param ... additional arguments for formatting the histogram
+#'
+#' @return returns a list containing the confidence interval, the function used, and the sample
+#'
+#' @export
+#'
+nathanboot2<-function(iter=10000,x,fun="mean",alpha=0.05,cx=1.5,...){  #Notice where the ... is repeated in the code
+  n=length(x)   #sample size
+
+  y=sample(x,n*iter,replace=TRUE)
+  rs.mat=matrix(y,nr=n,nc=iter,byrow=TRUE)
+  xstat=apply(rs.mat,2,fun) # xstat is a vector and will have iter values in it
+  ci=quantile(xstat,c(alpha/2,1-alpha/2))# Nice way to form a confidence interval
+  # A histogram follows
+  # The object para will contain the parameters used to make the histogram
+  para=hist(xstat,freq=FALSE,las=1,
+            main=paste("Histogram of Bootstrap sample statistics","\n","alpha=",alpha," iter=",iter,sep=""),
+            ...)
+
+  #mat will be a matrix that contains the data, this is done so that I can use apply()
+  mat=matrix(x,nrow=length(x),ncol=1,byrow=TRUE)
+
+  #pte is the point estimate
+  #This uses whatever fun is
+  pte=apply(mat,2,fun)
+  abline(v=pte,lwd=3,col="Black")# Vertical line
+  segments(ci[1],0,ci[2],0,lwd=4)      #Make the segment for the ci
+  text(ci[1],0,paste("(",round(ci[1],2),sep=""),col="Red",cex=cx)
+  text(ci[2],0,paste(round(ci[2],2),")",sep=""),col="Red",cex=cx)
+
+  # plot the point estimate 1/2 way up the density
+  text(pte,max(para$density)/2,round(pte,2),cex=cx)
+
+  invisible(list(ci=ci,fun=fun,x=x))# Some output to use if necessary
+}
