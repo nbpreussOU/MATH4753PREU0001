7500561690324b8536665703ee03e1f89890362d
Nathan Preuss <nathan.b.preuss-1@ou.edu>
2020-11-17 14:19:54 -0600

    lab 12

---
+++ R/nathanpvalue.R
@@ -0,0 +1,44 @@
+#' Create various plots for CLT of a poisson distribution.
+#'
+#' @param t0 the calculated t statistic
+#' @param xmax the highest possible t statistic
+#' @param n the number of samples to create
+#' @param alpha alpha value for a 1-alpha percent confidence interval
+#' @return This function returns a graph of the t statistic with the sample mean and the area described, and a list containing the bounds on the rejection region as well as the p value
+#'
+#' @export
+#'
+nathanpvalue=function(t0,xmax=4,n=20, alpha=0.05){
+  #calculate alpha/2
+  va=round(pt(-t0,df=n-1),4)
+  pv=2*va
+
+  # plot the t dist
+  curve(dt(x,df=n-1),xlim=c(-xmax,xmax),ylab="T Density",xlab=expression(t),
+        main=substitute(paste("P-value=", pv, " alpha=", alpha)))
+
+
+  # set up points on the polygon to the right
+  xcurve=seq(t0,xmax,length=1000)
+  ycurve=dt(xcurve,df=n-1)
+
+  # set up points to the left
+  xlcurve=seq(-t0,-xmax,length=1000)
+  ylcurve=dt(xcurve,df=n-1)
+
+  # Shade in the polygon defined by the line segments
+  polygon(c(t0,xcurve,xmax),c(0,ycurve,0),col="green")
+  polygon(c(-t0,xlcurve,-xmax),c(0,ylcurve,0),col="green")
+
+  # make quantiles
+  q=qt(1-alpha/2,n-1)
+  abline( v=c(q,-q),lwd=2) # plot the cut off t value
+  axis(3,c(q,-q),c(expression(abs(t[alpha/2])),expression(-abs(t[alpha/2]))))
+
+
+  # Annotation
+  text(0.5*(t0+xmax),max(ycurve),substitute(paste(area, "=",va)))
+  text(-0.5*(t0+xmax),max(ycurve),expression(area))
+
+  return(list(q=q,pvalue=pv))
+}
